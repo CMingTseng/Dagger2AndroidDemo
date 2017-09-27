292e6bfd79854e5ad175c381f4ca2a046d52016b
Lany <lany192@qq.com>
2017-09-27 15:58:04 +0800

    Initial commit

---
+++ app/src/main/java/com/github/lany192/dagger2/demo/MyApp.java
@@ -0,0 +1,10 @@
+package com.github.lany192.dagger2.demo;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/9/27 0027.
+ */
+
+public class MyApp extends Application {
+}
